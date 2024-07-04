9378d437388791c8cc4f93818e09c5a76d3d4163
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-07-04 10:22:40 +0900

    useState 사용

---
+++ tutorial-one/src/R05_Props3.js
@@ -0,0 +1,12 @@
+const 부모3번 = (props) => {
+    const {num, name, age, gender, phone} = props
+
+    return (
+
+        <div className="info-ex">
+            번호 : {num} / 이름 : {name} / 나이 : {age} / 성별 : {gender} / 핸드폰번호 : {phone}
+        </div>
+    )
+}
+
+export default 부모3번;
