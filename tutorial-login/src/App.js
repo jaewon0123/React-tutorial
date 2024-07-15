ad831fe39b5f86f60953f19e5eb82cbc7f9e305b
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-07-15 17:51:27 +0900

    password 확인 및 아이디중복검사

--- tutorial-login/src/App.js
+++ tutorial-login/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import LoginContext from './components/LoginContext';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import './App.css';
 
 /* 현재 App.js는 제일 위에 있는 컴포넌트 = 객체 */
 function App() {
