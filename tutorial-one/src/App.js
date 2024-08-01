0959f9a7d124a26f838b46d54eafd0f8df2abfbc
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-08-01 10:39:55 +0900

    유저 목록 추가, 삭제

--- tutorial-one/src/App.js
+++ tutorial-one/src/App.js
@@ -43,6 +43,7 @@ import Fetch예제1번 from './component/R28_Fetch_ex.js';
 import Fetch예제2번 from './component/R29_Fetch_Ex2.js';
 import Board from './component/R30_PageNation/Board.js';
 import Album from './component/R31_PagiNationEx/Album.js';
+import 프로필 from './component/R32_Profile.js';
 function App() {
   return (
     /*
@@ -91,7 +92,8 @@ function App() {
         {/*<Fetch예제1번/>*/}
         {/*<Fetch예제2번/>*/}
         {/*<Board/>*/}
-        <Album/>
+        {/*<Album/>*/}
+        <프로필/>
     </div>
   );
 }
